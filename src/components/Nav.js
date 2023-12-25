3c5fea2d35f06777f27069aa890d587bb27ce69c
kushalkumar09 <kushal1748.be21@chitkara.edu.in>
2023-12-25 13:28:20 +0530

    authentication done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,32 +1,71 @@
 // Header.js
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [username, setUsername] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:4000/api/v1/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        setUsername(userInfo.username);
+      });
+    });
+  }, []);
+
   return (
-    <header className="header">
-      <div className="logo-container">
+    <header className="header md:px-20">
+      <div className="logo-container md:ml-6">
         {/* <Logo /> */}
-        <Link to='/'>Logo</Link>
+        <Link to="/">Logo</Link>
       </div>
       <nav>
-        <ul className="nav-list">
-          <li>
-            <Link
-              to="/register"
-              className="nav-link"
-              onClick={() => console.warn(window.location.protocol)}
-            >
-              Register
-            </Link>
-          </li>
-          <li>
-            <Link to="/login" className="nav-link">
-              Login
-            </Link>
-          </li>
-        </ul>
+        {username ? (
+          <>
+            <ul className="nav-list">
+              <li>
+                <Link
+                  to="/create"
+                  className="nav-link"
+                  onClick={() => console.warn(window.location.protocol)}
+                >
+                  create Post
+                </Link>
+              </li>
+              <li>
+                <Link to="/" className="nav-link font-thin ">
+                  {username}
+                </Link>
+              </li>
+              <li>
+                <Link to="/logout" className="nav-link">
+                  LogOut
+                </Link>
+              </li>
+            </ul>
+          </>
+        ) : (
+          <>
+            <ul className="nav-list">
+              <li>
+                <Link
+                  to="/register"
+                  className="nav-link"
+                  onClick={() => console.warn(window.location.protocol)}
+                >
+                  Register
+                </Link>
+              </li>
+              <li>
+                <Link to="/login" className="nav-link">
+                  Login
+                </Link>
+              </li>
+            </ul>
+          </>
+        )}
       </nav>
     </header>
   );
--- src/components/Post.js
+++ src/components/Post.js
@@ -3,15 +3,15 @@ import React from "react";
 function Post() {
   return (
     <>
-      <div className="w-full md:flex md:mt-5 md:p-5 p-3 ">
-        <div className="sm:h-80 overflow-hidden flex justify-center md:w-1/2 md:p-4 ">
+      <div className="w-full md:flex md:mt-5 md:p-5 p-3">
+        <div className="h-80 overflow-hidden flex justify-center md:w-1/2 md:p-4 ">
           <img
-            src="https://images.unsplash.com/photo-1648413653877-ade5eefd2f1b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8"
+            src="https://images.unsplash.com/photo-1613835842551-894cdd236160?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="sky-skeeper"
             className="object-contain md:max-h-96 md:max-w-md "
           />
         </div>
-        <div className="md:flex-1 md:p-6 mt-4 p-3 text-justify">
+        <div className="md:h-80 md:flex-1 overflow-hidden p-3 text-justify md:w-1/2 md:p-4 ">
           <h2 className="text-2xl font-bold mb-2 bg-zinc-200 uppercase">
             Heading lorem10
           </h2>
